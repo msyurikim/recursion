e354763cf036b4f008614eeff60cdfbeaa11f3fa
Yuri Kim <yurikim123@gmail.com>
2020-03-11 13:33:22 -1000

    Update stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'undefined') return;
+  if (typeof obj === 'function') return;
+  if (obj === null) return 'null';
+  if (typeof obj === 'boolean' || typeof obj === 'number' || typeof obj === 'symbol') return '' + obj + '';
+  if (typeof obj === 'string') return '"' + obj + '"';
+
+
+  //use recursion to handle nested array/object
+  //input is array
+  if (Array.isArray(obj)) {
+    /** 
+     * similar solution to example using while loop:
+    var str = '[';
+    while (obj.length) {
+      str += stringifyJSON(obj.shift());
+      if (obj.length !== 0 ) str += ',';
+    }
+    return str + ']';
+    */
+    
+    //array -> array of strings (using recursion + map) 
+    //array --> 1 string, join w/ comma (default)
+    return '[' + obj.map(o => stringifyJSON(o)).join() + ']';
+  }
+
+  //input is object, account for null object above
+  if (typeof obj === 'object') {
+    var str = '{';
+    for (var key in obj) {
+      if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') continue;
+      var pair = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      str += pair;
+      delete obj[key];
+      if (Object.keys(obj).length !== 0 ) str += ',';
+    }
+    return str + '}';
+  }
+
 };
